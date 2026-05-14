f330501a89b6f2d888e6d3978811decf15f9d480
Steven Clontz <steven.clontz@gmail.com>
2026-05-14 17:59:18 +0000

    escape markdown string

--- app/models/project.rb
+++ app/models/project.rb
@@ -112,7 +112,7 @@ class Project < ApplicationRecord
     This is a sample project to get you started. You can edit this content using _PreTeXt Markdown_.
 
     $$
-      \left|\sum_{i=0}^n a_i\right| \leq \sum_{i=0}^n |a_i|
+      \\left|\\sum_{i=0}^n a_i\\right| \\leq \\sum_{i=0}^n |a_i|
     $$
 
     For more information, visit https://pretextbook.org/doc/guide/html/.
@@ -120,7 +120,7 @@ class Project < ApplicationRecord
     Feel free to delete this sample content and start creating your own project. Happy writing!
 
     Theorem:
-      PreTeXt can be used in markdown mode.
+      PreTeXt can be used in Markdown mode.
 
       Proof:
         See this document.
